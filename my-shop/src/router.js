d4c303c89dcdc0a569425feed20af1ce7ebf12c2
Sertrydel <Sertrydel@gmail.com>
2026-08-26 20:05:19 +0300

    Update router.js

--- my-shop/src/router.js
+++ my-shop/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import AdminDashboard from '@/pages/AdminDashboard.vue';
 import ManageProducts from '@/pages/ManageProducts.vue';
 import ManageReviews from '@/pages/ManageReviews.vue';
@@ -29,7 +29,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory('my-shop'),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
